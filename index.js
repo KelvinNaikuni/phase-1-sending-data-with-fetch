e3331aaad1a69a7e3ede4746f54192a9c09af42f
KelvinNaikuni <kelvin.naikuni@student.moringaschool.com>
2023-10-06 22:03:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(name, email) {
+  const userData = {
+    name: name,
+    email: email
+  };
+  fetch("http://localhost:3000/users", configObject);
+  const configObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(userData)
+  };
+  return fetch("http://localhost:3000/users", configObject)
+  .then(response => response.json())
+  .then(data => {
+    // Append the new ID to the DOM
+    document.body.innerHTML = data.id;
+  })
+  .catch(error => {
+    // Handle and display errors
+    document.body.innerHTML = error.message;
+  });
+}
+
+
+
+
+
